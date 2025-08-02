1372705169d1ec8b2a8875fc821c8740190e2e58
wallche <pacherazova@gmail.com>
2025-08-02 15:12:12 +0200

    Fix problems

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,19 +25,7 @@ html {
 }
         `}</style>
       </head>
-      <body className="bg-gradient-to-br from-slate-50 to-blue-50">{children}
-        <script type="text/javascript">
-          (function(l,e,a,p) {
-            if (window.Sprig) return;
-            window.Sprig = function(){S._queue.push(arguments)}
-            var S = window.Sprig;S.appId = a;S._queue = [];window.UserLeap=S;
-            a=l.createElement('script');
-            a.async=1;a.src=e+'?id='+S.appId;
-            p=l.getElementsByTagName('script')[0];
-            p.parentNode.insertBefore(a, p);
-          })(document, 'https://cdn.sprig.com/shim.js', 'R69zSijMMoql');
-        </script>
-      </body>
+      <body className="bg-gradient-to-br from-slate-50 to-blue-50">{children}</body>
     </html>
   )
 }
